15816897c96fd839bc981da17e153dd7d2d465a8
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-08 11:12:25 +0500

    for loops in typescript

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+// for loop 
+// syntax 
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+for (let i = 1; i <= 5; i++) {
+    console.log(`${i}: Hey! This is me AREEBA HAMMAD`);
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(`2 x ${i} = `, i * 2);
+}
---
+++ index.ts
@@ -0,0 +1,16 @@
+// for loop 
+// syntax 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for(let i: number = 1; i <= 5; i++){
+    console.log(`${i}: Hey! This is me AREEBA HAMMAD`);
+}
+
+for(let i: number = 1; i <= 10; i++){
+    console.log(`2 x ${i} = `, i * 2);
+}
+
